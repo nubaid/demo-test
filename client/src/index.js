fceb689d6d47bd7ec65a04c89d7523e9614428bb
nubaid <navaid.khan69@gmail.com>
2019-12-18 13:54:50 +0530

    added apollo client in index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+// import { createUploadLink } from "apollo-upload-client";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+import { setContext } from "apollo-link-context";
+import { HttpLink } from "apollo-link-http";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// create a http link
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
+// // An Apollo Link for error handling
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  console.log("graphql err: ", graphQLErrors);
+  console.log("network err: ", networkError);
+
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      );
+    });
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+// contact httplink with error link
+const link = errorLink.concat(httpLink);
+
+// fetchPolicy as network-only avoids using the cache.
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "cache-and-network",
+    errorPolicy: "ignore"
+  },
+  query: {
+    fetchPolicy: "network-only",
+    errorPolicy: "all"
+  }
+};
+
+// creating an instance of Apollo Client
+const client = new ApolloClient({
+  link: ApolloLink.from([link]),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
